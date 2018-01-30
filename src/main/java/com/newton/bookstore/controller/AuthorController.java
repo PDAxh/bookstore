196eda26523edcbe44a01cb33f7aac541b03435d
Daniel <axhammar.daniel@gmail.com>
2018-01-30 16:02:10 +0100

    made connection to add and view authors

--- src/main/java/com/newton/bookstore/controller/AuthorController.java
+++ src/main/java/com/newton/bookstore/controller/AuthorController.java
@@ -4,11 +4,12 @@ package com.newton.bookstore.controller;
 import com.newton.bookstore.entity.Author;
 import com.newton.bookstore.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Scanner;
+
 
 @RestController
 @RequestMapping("/api")
@@ -18,20 +19,38 @@ public class AuthorController {
     AuthorRepository authorRepository;
 
     @GetMapping("/authors")
-    public List<Author> getAllNotes() {
+    public List<Author> getAllAuthors() {
         return authorRepository.findAll();
     }
 
 
     @PostMapping("/addAuthor")
-    public Author createNote(@Valid @RequestBody Author author) {
+    public Author createAuthor(@Valid @RequestBody Author author) {
         return authorRepository.save(author);
     }
 
-    // Get a Single Note
+   @GetMapping("/notes/{id}")
+    public ResponseEntity<Author> getAuthorById(@PathVariable(value = "id") Long authorId) {
+        Author author = authorRepository.findOne(String.valueOf(authorId));
+        if(author == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(author);
+    }
 
     // Update a Note
 
     // Delete a Note
+    @DeleteMapping("/authors/{id}")
+    public ResponseEntity<Author> deleteAuthor(@PathVariable(value = "id") Long authorId) {
+        Author author = authorRepository.findOne(String.valueOf(authorId));
+        if(author == null) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            authorRepository.delete(author);
+        }
+        return ResponseEntity.ok().build();
+    }
 
 }
---
+++ src/main/java/com/newton/bookstore/controller/BookController.java
@@ -0,0 +1,4 @@
+package com.newton.bookstore.controller;
+
+public class BookController {
+}
