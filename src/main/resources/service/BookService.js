d62db3aaa0b621e68a927415d445d55ce92acd64
Atise <joseflundstrom@gmail.com>
2018-02-01 12:56:39 +0000

    Service javascript

---
+++ src/main/resources/service/BookService.js
@@ -0,0 +1,12 @@
+
+/*
+
+var input = document.getElementById('hiddenInput');
+input.value = testVariable.toString();
+
+
+
+var title = document.getElementById('InputTitle');
+title.value = titleVariable.toString();
+
+*/
