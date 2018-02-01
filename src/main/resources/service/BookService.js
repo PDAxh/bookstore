7d03c7b02d7d761802ca16978f4adf37e9ce8ace
Atise <joseflundstrom@gmail.com>
2018-02-01 13:59:20 +0000

    BookService.js

--- src/main/resources/service/BookService.js
+++ src/main/resources/service/BookService.js
@@ -1,14 +1,21 @@
+$(function(){
 
+var data = {};
+data.title = $('#InputTitle');
+data.genre = $('#InputGenre');
+data.published = $('#InputPub');
+data.price = $('#InputPrice');
+data.inventory = $('#InputInv');
+data.bookAuthor = $('#InputAuthor');
 
+$.ajax({
+    type: 'POST',
+    data: JSON.stingify(data),
+    contentType: 'application/json',
+    url: 'http://localhost:3300/api/addBook',
+    success:function(data){
+        console.log('success');
+        console.log(JSON.stringify(data));
+    }
+});
 
-var title = $('#InputTitle');
-
-var genre = $('#InputGenre');
-
-var published = $('#InputPub');
-
-var price = $('#InputPrice');
-
-var inventory = $('#InputInv');
-
-var BookAuthor = $('#InputAuthor');
