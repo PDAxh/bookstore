1a24e0e50b8664cd8f37dc924655a8c041dcc4c0
Unknown <joseflundstrom@gmail.com>
2018-02-06 13:29:38 +0100

    Added function to add autohors

--- src/main/resources/static/js/createBook.js
+++ src/main/resources/static/js/createBook.js
@@ -1,3 +1,29 @@
+var xhr = new XMLHttpRequest();
+var responseObject;
+var dropdownList = document.getElementById("selectAuthor");
+var inputField = document.getElementById("inputAuthor");
+
+
+xhr.onload = function() {
+    if(xhr.status == 200) {
+        responseObject = JSON.parse(xhr.responseText);
+        console.log(responseObject);
+        popAuthorList();
+    }
+};
+
+xhr.open('GET', 'http://localhost:3300/api/authors', true);
+xhr.send(null);
+
+function popAuthorList(){
+    for(var i = 0; i < responseObject.length; i++){
+        var option = document.createElement('option');
+        option.innerHTML = responseObject[i].name;
+        option.value = responseObject[i].id;
+        dropdownList.appendChild(option);
+    }
+}
+
 function createAuthor() {
     var xhr = new XMLHttpRequest();
 
