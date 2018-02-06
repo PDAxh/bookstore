483aba4ec24c7dc757d580b0d9c7da717fe73354
drybone <jackie_chan1994@hotmail.com>
2018-02-06 12:02:48 +0100

    Added RatingController and RatingRepository

---
+++ src/main/java/com/newton/bookstore/controller/RatingController.java
@@ -0,0 +1,17 @@
+package com.newton.bookstore.controller;
+
+import com.newton.bookstore.entity.Book;
+import com.newton.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+public class RatingController {
+
+}
---
+++ src/main/java/com/newton/bookstore/repository/RatingRepository.java
@@ -0,0 +1,10 @@
+package com.newton.bookstore.repository;
+
+import com.newton.bookstore.entity.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Integer> {
+
+}
