498e3bc2ba6dad4ed98514557df981128d8fcc31
Unknown <joseflundstrom@gmail.com>
2018-02-07 12:26:25 +0100

    addbook fix ???

--- src/main/resources/static/js/addBook.js
+++ src/main/resources/static/js/addBook.js
@@ -6,14 +6,38 @@ xhr.onload = function () {
         console.log(responseObject);
         createAddBookTable(responseObject);
     }
-}
+};
+
+var title = document.getElementById("InputTitle").value;
+var genre = document.getElementById("InputGenre").value;
+var published = document.getElementById("InputPub").value;
+var price = document.getElementById("InputPrice").value;
+var inventory = document.getElementById("InputInv").value;
+var author = document.getElementById("InputAuthor").value;
+
+console.log(title);
+console.log(genre);
+console.log(published);
+console.log(price);
+console.log(inventory);
+console.log(author);
+
+
 
 xhr.open('GET', 'http://localhost:3300/api/books', true);
 xhr.send(null);
 
 function createAddBookTable(data) {
     console.log(data);
-    var addBookTable = '<table class="table"><thead><tr><th scope="col">Title</th><th scope="col">Genre</th><th scope="col">Published</th><th scope="col">Price</th><th scope="col">Inventory</th><th scope="col">Author</th><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></th><tr></thead><tbody>';
+    var addBookTable = '<table class="table"><thead><tr>' +
+        '<th scope="col">Title</th>' +
+        '<th scope="col">Genre</th>' +
+        '<th scope="col">Published</th>' +
+        '<th scope="col">Price</th>' +
+        '<th scope="col">Inventory</th>' +
+        '<th scope="col">Author</th>' +
+        '<span class="glyphicon glyphicon-wrench" aria-hidden="true"></span>' +
+        '</th><tr></thead><tbody>';
 
     $.each(data, function (key, item) {
         addBookTable += '<tr><td scope="row"><p>'
