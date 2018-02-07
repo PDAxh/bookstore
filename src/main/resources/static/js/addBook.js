53b995adccd15923eabda992d77d3c47954db173
Unknown <joseflundstrom@gmail.com>
2018-02-07 15:27:18 +0100

    work on endpoint

--- src/main/resources/static/js/addBook.js
+++ src/main/resources/static/js/addBook.js
@@ -11,6 +11,9 @@ xhr.onload = function () {
 xhr.open('GET', 'http://localhost:3300/api/authors', true);
 xhr.send(null);
 
+
+
+
 function addBook() {
     var title = document.getElementById("InputTitle").value;
     var genre = document.getElementById("InputGenre").value;
@@ -19,5 +22,18 @@ function addBook() {
     var inventory = document.getElementById("InputInv").value;
     var author = document.getElementById("InputAuthor").value;
 
-    var data = JSON.parse();
+    var data = JSON.stringify({"title": title, "genre": genre, "published": published, "price": price, "inventory": inventory, "author": author });
+
+
+    xhr.open('POST', 'http://localhost:3300/api/addAuthor', true);
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.onreadystatechange = function () {
+        if( xhr.readyState=== 4 && xhr.status===200){
+            var json = JSON.parse(xhr.responseText);
+            console.log(json.name )
+
+        }
+
+    };
+    xhr.send(data);
 }
