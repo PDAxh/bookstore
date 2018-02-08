5efee4af6410a9e939e2b51286c38f075f6ac85d
Andreas Lindgren <c.andreas.lindgren@gmail.com>
2018-02-08 18:56:37 +0100

    final changes 2

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -14,22 +14,19 @@ xhr.open('GET', 'http://localhost:3300/api/books', true);
 xhr.send(null);
 
 function createBookTable(data) {
-    console.log(data);
-    var bookTable = '<table class="table table-striped"><thead><tr><th scope="col">Title</th><th scope="col">Genre</th><th scope="col">Author</th><th scope="col">Published</th><th scope="col">Price</th><th>Rating</th><th scope="col">Inventory<th scope="col"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></th><tr></thead><tbody>';
+    var bookTable = '<table class="table table-striped"><thead><tr><th scope="col">Title</th><th scope="col">Genre</th><th scope="col">Author</th><th scope="col">Published</th><th scope="col">Price</th><th scope="col">Inventory<th scope="col"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></th><tr></thead><tbody>';
 
     $.each(data, function (key, item) {
 
-        bookTable += '<tr><td scope="row"><p class="bold">' + item.title + '</p></td><td scope="row"><p>' + item.genre + '</p></td><td scope="row"><p>' + item.author + '</p></td><td scope="row"><p>' + item.publishedYear + '</p></td><td scope="row"><p>' + item.price + '</p></td><td scope="row"><p>' + item.rating + '</p></td><td scope="row"><p>' + item.inventory + '</p></td><td scope="row">   <button type="button" title="Add Up" class="btn btn-default" value="'+ item.id +'" onclick="addInv(this.value)"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button><button type="button" title="Add Down" class="btn btn-default" value="'+ item.id +'" onclick="removeInv(this.value)"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button><a href="editBook.html"><button type="button" title="Edit Book" class="btn btn-primary" aria-label="Left Align" value="'+item.id+'" onclick="transferId(this.value)"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></a><button type="button" title="Delete Book" class="btn btn-danger" aria-label="Left Align" value="' + item.id +'" onclick="deleteBook(this.value)"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button><button type="button" title="Rate Book" class="btn btn-warning" aria-label="Left Align" data-toggle="modal" href="#myModal" value="' + item.id +'" onclick="setBookToRate(this.value)"><span class="glyphicon glyphicon-star" aria-hidden="true"></span></button><a href="ratingStatistics.html"><button type="button" title="Statistics" class="btn btn-info" aria-label="Left Align" value="'+item.id+'" onclick="transferId(this.value)"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span></button></a></td></tr>';
+        bookTable += '<tr><td scope="row"><p class="bold">' + item.title + '</p></td><td scope="row"><p>' + item.genre + '</p></td><td scope="row"><p>' + item.author + '</p></td><td scope="row"><p>' + item.publishedYear + '</p></td><td scope="row"><p>' + item.price + '</p></td><td scope="row"><p>' + item.inventory + '</p></td><td scope="row">   <button type="button" title="Add Up" class="btn btn-default" value="'+ item.id +'" onclick="addInv(this.value)"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button><button type="button" title="Add Down" class="btn btn-default" value="'+ item.id +'" onclick="removeInv(this.value)"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button><a href="editBook.html"><button type="button" title="Edit Book" class="btn btn-primary" aria-label="Left Align" value="'+item.id+'" onclick="transferId(this.value)"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></a><button type="button" title="Delete Book" class="btn btn-danger" aria-label="Left Align" value="' + item.id +'" onclick="deleteBook(this.value)"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button><button type="button" title="Rate Book" class="btn btn-warning" aria-label="Left Align" data-toggle="modal" href="#myModal" value="' + item.id +'" onclick="setBookToRate(this.value)"><span class="glyphicon glyphicon-star" aria-hidden="true"></span></button><a href="ratingStatistics.html"><button type="button" title="Statistics" class="btn btn-info" aria-label="Left Align" value="'+item.id+'" onclick="transferId(this.value)"><span class="glyphicon glyphicon-stats" aria-hidden="true"></span></button></a></td></tr>';
 
     });
-    console.log(bookTable);
     bookTable += '</tbody></table>';
     $('#bookTableContainer').html(bookTable);
 }
 
 function setBookToRate(value) {
     bookToRate = value;
-    console.log(value);
 }
 
 function rateBook(rating) {
@@ -79,15 +76,12 @@ function deleteBook(id) {
             url: 'http://localhost:3300/api/deleteBooks/' + id,
             type: 'DELETE',
             success: function (result) {
-                console.log("Book has been deleted");
                 location.reload();
             }
         });
 }
 
 function updateInv(id, type) {
-
-    console.log(type);
 
     var bookToPut = new Object;
 
@@ -96,20 +90,18 @@ function updateInv(id, type) {
             bookToPut = {
                 "title": responseObject[i].title,
                 "author": responseObject[i].author,
+                "authorId" : responseObject[i].authorId,
                 "genre": responseObject[i].genre,
                 "publishedYear": responseObject[i].publishedYear,
-                "rating": responseObject[i].rating,
                 "price": responseObject[i].price,
                 "inventory": responseObject[i].inventory
             };
             if (type == "add") {
                 bookToPut.inventory++;
-                console.log(bookToPut);
             } else if (type == "remove") {
                 bookToPut.inventory--;
                 if (bookToPut.inventory < 0)
                     bookToPut.inventory = 0;
-                console.log(bookToPut);
             } else {
                 console.log("Something went wrong!");
             }
@@ -123,8 +115,6 @@ function updateInv(id, type) {
         contentType: 'application/json',
         data: JSON.stringify(bookToPut),
         success: function (data, textStatus, xhr) {
-            console.log("Entity successfully saved");
-            console.log(data);
             location.reload();
         },
         error: function (xhr, textStatus, errorThrown) {
