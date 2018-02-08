b3eb3449f437a7139b8751cd94874246232aac4c
Andreas Lindgren <c.andreas.lindgren@gmail.com>
2018-02-08 11:31:22 +0100

    edit book

--- src/main/resources/static/js/editBook.js
+++ src/main/resources/static/js/editBook.js
@@ -40,7 +40,7 @@ function popAuthorList(){
             option.id = authorObject[i].id;
             console.log(authorObject[i].name + authorObject[i].id);
             dropdownList.appendChild(option);
-            
+
             if(option.id == bookObject.authorId){
                 dropdownList.selectedIndex = option.index;
             }
@@ -55,4 +55,33 @@ function fillForm(){
     document.getElementById("InputPub").value = bookObject.publishedYear;
     document.getElementById("InputPrice").value = bookObject.price;
     document.getElementById("InputInv").value = bookObject.inventory;
+}
+
+function updateBook() {
+    var title = document.getElementById("InputTitle").value;
+    var genre = document.getElementById("InputGenre").value;
+    var publishedYear = parseInt(document.getElementById("InputPub").value);
+    var rating = parseInt(bookObject.rating);
+    var author = authorId = dropdownList.options[dropdownList.selectedIndex].innerHTML;
+    var price = parseInt(document.getElementById("InputPrice").value);
+    var inventory = parseInt(document.getElementById("InputInv").value);
+    var authorId = parseInt(dropdownList.options[dropdownList.selectedIndex].id);
+
+    var package = {"title" : title, "genre" : genre, "publishedYear" : publishedYear, "rating" : rating, "price" : price, "author" : author, "inventory" : inventory, "authorId" : authorId};
+    console.log(package);
+
+    $.ajax({
+            url: 'http://localhost:3300/api/updateBook/' + id,
+            type: 'PUT',
+            dataType: 'json',
+            contentType : 'application/json',
+            data: JSON.stringify(package),
+            success: function (data, textStatus, xhr) {
+                console.log("Entity successfully saved");
+                console.log(data);
+            },
+            error: function (xhr, textStatus, errorThrown) {
+                console.log('Error in Operation');
+            }
+        });
 }
